93f221882337de806f360499c8eb95c331df223e
anasahmed-10p <anas.ahmed@10pearls.com>
2021-08-05 07:18:32 +0500

    states

--- src/components/Expenses/ExpanseItem.js
+++ src/components/Expenses/ExpanseItem.js
@@ -1,17 +1,26 @@
 import "./ExpanseItem.css";
 import ExpanseDate from "./ExpanseDate.js"
 import Card from '../UI/Card';
+import React,{useState} from "react";
 
 
 
 function ExpanseItem(props) {
+   const [title,setTitle] =useState(props.title);
+
+   const clickHandeler=()=>{
+       setTitle('updated');
+       console.log('updated');
+
+   }
     return (
         <Card className='expense-item'>
             <ExpanseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{props.amount}</div>
             </div>
+            <button onClick={clickHandeler}></button>
         </Card>
     );
 }
